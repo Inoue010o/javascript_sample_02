17dfafbe51d2eaee3a28153a1830bd61efefb212
Inoue010o <167289123+Inoue010o@users.noreply.github.com>
2024-06-12 16:59:05 +0900

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,15 @@
+//背景色を変える
+function MyFunctionR(){
+    const obj = document.body;
+    obj.style.backgroundColor = 'red';
+  }
+  
+  function MyFunctionG(){
+    const obj = document.body;        
+    obj.style.backgroundColor = 'green';
+  }
+  
+  function MyFunctionB(){
+    const obj = document.body;
+    obj.style.backgroundColor = 'blue';
+  }
